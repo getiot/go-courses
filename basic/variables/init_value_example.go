80614eb5f569085d6479b9e1b4358ce74a5fa005
luhuadong <luhuadong@163.com>
2024-04-11 23:42:26 +0800

    add variable init value example

---
+++ basic/variables/init_value_example.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    var num int
+    var flag bool
+    var str string
+    var ptr *int
+
+    fmt.Println("num:", num)   // 输出 0
+    fmt.Println("flag:", flag) // 输出 false
+    fmt.Println("str:", str)   // 输出 空字符串
+    fmt.Println("ptr:", ptr)   // 输出 nil
+}
